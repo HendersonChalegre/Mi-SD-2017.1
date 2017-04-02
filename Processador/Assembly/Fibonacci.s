d138dc2824887151412d9a4a658e9789afa12828
HendersonChalegre <hendersonchalegre@gmail.com>
2017-04-02 14:19:48 -0300

    Fibonacci
    Funcionando

--- Processador/Assembly/Fibonacci.s
+++ Processador/Assembly/Fibonacci.s
@@ -6,17 +6,17 @@
 		.global main
 		.text
 		
-main:		movia r0, 0
-		movia r1, 1
-		movia r10,4               # coloca o valor maximo
+main:		movia r5, 0
+		movia r6, 1
+		movia r10,10              # coloca o valor maximo
 		movia r11, 2	          # contador			
 		movia r12, 0	          # primeira posição	
 		movia r13, 1	          # segunda posicao
 		
 			
-		#bgt r10, r0, LA
-		#bgt r10, r1, LA
-		#bgt r10, r11, LB
+		ble r10, r5, LA		  # verifica se a posição maxima é a zero
+		ble r10, r6, LA		  # verifica se a posição maxima é a um
+		ble r10, r11, LB	  # verifica se a posição maxima é a dois
 		
 		
 loop:		addi r11, r11,1	          # contador incrementa 	
@@ -29,7 +29,7 @@ loop:		addi r11, r11,1	          # contador incrementa
 		
 		br loop		          # retorna o loop		
 
-LA:		movia r14, 0	          # coloca no reg de saida o valor da primeira posição	
+LA:		movia r14, 0              # coloca no reg de saida o valor da primeira posição	
 		br fim		   	  # pula pro fim
 		
 LB: 		movia r14, 1		  # coloca no reg de saida o valor da segunda posição	

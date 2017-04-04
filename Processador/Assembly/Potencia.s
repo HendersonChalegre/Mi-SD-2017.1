48a0af42b85a7d45d49e22dcc419e44c1f29660e
Henderson Chalegre <hendersonchalegre@gmail.com>
2017-04-04 14:58:45 -0300

    Exponencial
    Funcioando

--- Processador/Assembly/Potencia.s
+++ Processador/Assembly/Potencia.s
@@ -1,5 +1,5 @@
 ###############################################
-# Fatorial
+# Exponencial
 ###############################################
 
 		.data
@@ -8,18 +8,31 @@
 		
 main:	movia r5, 0
 	movia r6, 1	
-	movia r10, 4		# base
+	movia r10, 8		# base
 	movia r11,2		# expoente
-	movia r12,1    		# contador
+	movia r15,0
 		
 	ble r10, r5, LA		# base = 0
 	ble r10, r6, LB		# base = 1
 	ble r11, r5, LB		# expoente = 0
 	ble r11, r6, LC		# expoente = 1
 	
+	mov r12, r11
+	mov r13, r10
+	subi r12,r12,1
+	mov r14, r10
+	subi r12,r12,1	
 		
-		
-loop:	#falta pensar nessa logica
+loop:	add r15, r15,r13
+	subi r14,r14,1
+	ble r14, r5, LH		
+	br loop
+
+
+LH:  ble r12, r5, fim		
+	mov r14, r12
+	subi r12,r12,1
+	br loop	
 
 LA:	movia r15, 0
 	br fim

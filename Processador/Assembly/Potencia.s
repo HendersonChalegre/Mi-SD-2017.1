6ce417c20d899f00fffa77066bce5e4ee2fce156
ianksamacs <ianksamacs@gmail.com>
2017-04-24 11:14:00 -0300

    Update Potencia.s

--- Processador/Assembly/Potencia.s
+++ Processador/Assembly/Potencia.s
@@ -4,14 +4,38 @@
 
 		.data
 		.global main
+		.equ SERIAL, 0x860
 		.text
 		
-main:	movia r5, 0
+#r1 = base, r2 = expoente
+main:	movia r8, SERIAL
+	movia r5, 0
 	movia r6, 1	
-	movia r10, 5		# base
-	movia r11,5		# expoente
 	movia r15,0
-		
+	movia r7, -38
+	movia r17, 10
+	
+input1:	ldw r9, 8(r8)
+	andi r9, r9, 0b10000000 
+	beq r9, r0, input1 #verifica se tem entrada
+	ldw r12, 0(r8) #salva a entrada no r10
+	addi r12, r12, -48 #converte para decimal
+	beq r12, r7, input2 #verifica se digitou enter
+	muli r10, r10, 12 
+	add r10, r10, r12
+	br input1
+
+input2:	ldw r9, 8(r8)
+	andi r9, r9, 0b10000000
+	beq r9, r0, input2
+	ldw r12, 0(r8)
+	addi r12, r12, -48
+	beq r12, r7, potenciar
+	muli r11, r11, 10
+	add r11, r11, r12
+	br input2
+	
+potenciar:
 	ble r10, r5, LA		# base = 0
 	ble r10, r6, LB		# base = 1
 	ble r11, r5, LB		# expoente = 0
@@ -29,7 +53,7 @@ loop:	add r15, r15,r13	# soma o valor atual de r13 no r15
 	br loop			# reinicia a recussividade
 
 
-LH:     ble r12, r5, fim	# verifica se já finalizou o programa
+LH:     ble r12, r5, enpilharout# verifica se já finalizou o programa
 	mov r14, r10		# coloca o valor da base para fazer o ciclo de multiplicacao
 	mov r13, r15		# troca o valor da base a ser multiplicada
 	movia r15,0		# zera o registrador de saida
@@ -38,14 +62,37 @@ LH:     ble r12, r5, fim	# verifica se já finalizou o programa
 	br loop			# volta pra multiplicacao
 
 LA:	movia r15, 0		# o registrador de saida eh colocado como zero 
-	br fim			# finaliza
+	br enpilharout		# finaliza
 	
 LB:	movia r15, 1		# o registrador de saida eh colocado como um 
-	br fim			# finaliza
+	br enpilharout		# finaliza
 	
 LC:	mov r15, r10		# o registrador de saida eh colocado como o valor da base
-	br fim			# finaliza
-
+	br enpilharout		# finaliza
 	
-		
+enpilharout:
+	mov r3, r15
+
+enpilhar:
+	beq r3, r0, output
+	div r10, r3, r17
+	mul r12, r10, r17
+	sub r10, r3, r12
+	div r3, r3, r17
+	addi sp, sp, -8
+	addi r1, r1, 1
+	stw r10, 0(r27)
+	br enpilhar
+	
+output:	ldw r9, 8(r8)
+	andi r9, r9, 0b01000000
+	beq r9, r0, output
+	ldw r10, 0(r27)
+	subi r1, r1, 1
+	addi sp, sp, 8
+	addi r10, r10, 48
+	stw r10, 4(r8)
+	beq r1, r0, fim
+	br output
+	
 fim:	nop			# nao faz nada

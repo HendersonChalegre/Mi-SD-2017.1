284feef4a77a4e9a6e4db915b6789453a5db864e
ianksamacs <ianksamacs@gmail.com>
2017-04-24 23:10:48 -0300

    Finalizado

--- Processador/Assembly/recurs-fac.s
+++ Processador/Assembly/recurs-fac.s
@@ -3,13 +3,13 @@
 .equ SERIAL, 0x860
 
 
-#Resultado estara em R2
+
 	
 main:
 	movia r8, SERIAL # R8 apontara para a entrada serial
 	movia r6, -38 #Enter
 	beq r2, r0, input
-	mov r1, r2
+	mov r1, r2 
 	movia r11, 10
 	movia r2, 1
 	
@@ -36,25 +36,25 @@ fatorial:
 trocar:	mov r3, r2
 	
 enpilharout:
-	beq r3, r0, output
-	div r10, r3, r11
-	mul r12, r10, r11
-	sub r10, r3, r12
-	div r3, r3, r11
-	addi sp, sp, -8
-	addi r4, r4, 1
-	stw r10, 0(r27)
+	beq r3, r0, output # Se resultado igual a zero vai para saida
+	div r10, r3, r11 # divide por 10
+	mul r12, r10, r11 
+	sub r10, r3, r12 #salva o resto da divisao
+	div r3, r3, r11 # salva o a saida -1 digito
+	addi sp, sp, -8 
+	addi r4, r4, 1 #quantidade de digitos
+	stw r10, 0(r27) # adciona na lista digito por digito
 	br enpilharout
 	
-output:	ldw r9, 8(r8)
+output:	ldw r9, 8(r8) 
 	andi r9, r9, 0b01000000
 	beq r9, r0, output
-	ldw r10, 0(r27)
-	subi r4, r4, 1
+	ldw r10, 0(r27) # lê da pilha um digito
+	subi r4, r4, 1 
 	addi sp, sp, 8
 	addi r10, r10, 48
-	stw r10, 4(r8)
-	beq r4, r0, exit
+	stw r10, 4(r8)  #envia para o UART o digito
+	beq r4, r0, exit # ate digito igual a > que 0
 	br output
 
 exit:
